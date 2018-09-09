1d05260c3bd9903353b1f35157918965e9ef7305
LeandroLuque <lealuque.tw@gmail.com>
2018-09-09 14:51:17 -0300

    pagination as react component

--- client/src/Resultados/Resultados.js
+++ client/src/Resultados/Resultados.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 import Loader from '../Spinners/Loader';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Product from '../Product/Product';
-import '../App.css'
+import Paginator from '../Paginator/Paginator'
+
 
 class Resultados extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {},
-      page: 1,
-      received: false,
       query: '',
       ajaxCompleted: false,
     };
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
+
   }
 
   componentDidMount() {
-    const offset = (this.state.page - 1) * 8;
     let query = this.props.match.params.query
-    fetch('https://server-ehygnedhic.now.sh/busqueda/' + query + '?limit=8&offset=' + offset)
+    let limit  = 8
+    let offset = 0
+    fetch('https://server-ehygnedhic.now.sh/busqueda/' + query + '?limit=' + limit +'&offset=' + offset)
       .then(data => {
         return data.json();
       })
@@ -31,59 +29,17 @@ class Resultados extends Component {
         console.log(result)
         this.setState({
           data: result,
-          total: result[0]['full_count'],
-          received: true,
+          total: (result.length ? result[0]['full_count'] : 0),
           query: query,
           ajaxCompleted: true
         });
       });
   }
 
-  changePage(page) {
-    let prods = [...this.state.data]
-    const offset = (page - 1) * 8;
-
-    fetch('https://server-ehygnedhic.now.sh/busqueda/' + this.state.query + '?limit=8&offset=' + offset)
-      .then(data => {
-        return data.json();
-      })
-      .then(result => {
-        this.setState({
-          data: result,
-          page: page
-        });
-      });
-  }
-
-  nextPage() {
-    if (this.state.page >= this.state.total / 8) {
-      this.setState({
-        page: this.state.page,
-      });
-    } else {
-      this.changePage(this.state.page + 1)
-    }
-  }
-
-  previousPage() {
-    if (this.state.page <= 1) {
-      this.setState({
-        page: 1,
-      });
-
-    } else {
-      this.changePage(this.state.page - 1)
-    }
-  }
-
   render() {
-    let { received } = this.state;
+
     let query = this.state.query
     let length = this.state.total
-    let prodslength = ''
-    let page = this.state.page
-
-    { this.state.received ? (prodslength = this.state.data.length) : ('') }
 
     return (
       
@@ -100,62 +56,14 @@ class Resultados extends Component {
               
             />
         ) : (
+          
+          <Paginator
+            url="'https://server-ehygnedhic.now.sh/busqueda/"
+            total={length}
+            query={query}
+            data={this.state.data}
+          />
         
-        <div>
-        <div className="container">
-          <div>
-            <br />
-            <br />
-            <h2 className="my-4">Resultados para "{query}"</h2>
-          </div>
-
-          <div className='quantity'>Página {page} | Mostrando {8 * (page - 1)} - {(8 * (page -1)) + prodslength} de {length} productos</div>
-
-          <ul className="pagination justify-content-center">
-            <li className="page-item">
-              <div className="page-link" onClick={this.previousPage}>
-                <span aria-hidden="true">&laquo;</span>
-                <span className="sr-only">Previous</span>
-              </div>
-            </li>
-            <li className="page-item">
-              <div className="page-link" onClick={this.nextPage}>
-                <span aria-hidden="true">&raquo;</span>
-                <span className="sr-only">Next</span>
-              </div>
-            </li>
-          </ul>
-
-          <div>
-            {received ? (
-              <div className="row">
-                {this.state.data.map((result, index) => {
-                  return <Product key={index} result={result} received={this.state.received} />;
-                })}
-              </div>
-            ) : (
-                ''
-              )}
-          </div>
-
-
-        </div>
-
-        <ul className="pagination justify-content-center">
-          <li className="page-item">
-            <div className="page-link" onClick={this.previousPage}>
-              <span aria-hidden="true">&laquo;</span>
-              <span className="sr-only">Previous</span>
-            </div>
-          </li>
-          <li className="page-item">
-            <div className="page-link" onClick={this.nextPage}>
-              <span aria-hidden="true">&raquo;</span>
-              <span className="sr-only">Next</span>
-            </div>
-          </li>
-        </ul>
-        </div>
         )}
         
         <Footer />
